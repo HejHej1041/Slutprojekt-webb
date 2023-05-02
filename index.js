4799ab0710ba334ea9fa2365b00fc11876d8067f
HejHej1041 <erik.lindstrom@elev.ga.ntig.se>
2023-05-02 16:37:47 +0200

    ss

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ myText.addEventListener("mouseover", function () {
 myText.addEventListener("mouseout", function () {
     myText.innerHTML = "Make your bathroom proud today";
 });
+
+const elemMobileMenu = document.querySelector(".mobile-menu")
+const elemPrimaryNavigation = document.querySelector(".primary-navigation")
+const iconHamburger = document.querySelector(".icon-hamburger")
+const btnHamburger = document.querySelector(".btn-hamburger")
+
+btnHamburger.addEventListener("click", () => {
+    elemPrimaryNavigation.classList.toggle("open");
+})
