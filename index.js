0c1bbab7084f23dc9079780e0fc79b6a3f398a33
HejHej1041 <erik.lindstrom@elev.ga.ntig.se>
2023-05-15 19:29:20 +0200

    ss

--- index.js
+++ index.js
@@ -17,6 +17,24 @@ btnHamburger.addEventListener("click", () => {
     elemPrimaryNavigation.classList.toggle("open");
 })
 
+window.addEventListener("scroll", function () {
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+
+})
+
+
+window.addEventListener("scroll", function () {
+    var navigation = document.querySelector(".navigation");
+    navigation.classList.toggle("sticky", window.scrollY > 0);
+
+})
+
+window.addEventListener("scroll", function () {
+    var primary_navigation = document.querySelector(".primary-navigation");
+    primary_navigation.classList.toggle("sticky", window.scrollY > 0);
+
+})
 
 
 
